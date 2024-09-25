3ef2faebe581b9b317d62fc6f260fb4a3a35fcac
Theodor-Felix Iacob <toyo.iacob@gmail.com>
2024-09-25 10:21:48 +0300

    vertical telescope

--- nvim2/.config/nvim2/lua/plugins/telescope.lua
+++ nvim2/.config/nvim2/lua/plugins/telescope.lua
@@ -30,6 +30,10 @@ return {
 						"--iglob",
 						"!.git",
 					},
+					layout_strategy = "vertical",
+					layout_config = {
+						preview_height = 0.65,
+					},
 				},
 				mapping = {
 					i = {
@@ -53,10 +57,33 @@ return {
 			local builtin = require("telescope.builtin")
 			local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
 			local find_files = function()
-				builtin.find_files {
-					find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' },
-					previewer = false
-				}
+				builtin.find_files({
+					find_command = { "rg", "--files", "--iglob", "!.git", "--hidden" },
+					previewer = true,
+				})
+			end
+			local lspOpts = {
+				fname_width = 0.4,
+				symbol_width = 0.2,
+				symbol_type_width = 0.1,
+			}
+			local lsp_document_symbols = function()
+				builtin.lsp_document_symbols(lspOpts)
+			end
+			local lsp_dynamic_workspace_symbols = function()
+				builtin.lsp_dynamic_workspace_symbols(lspOpts)
+			end
+			local lsp_definitions = function()
+				builtin.lsp_definitions(lspOpts)
+			end
+			local lsp_type_definitions = function()
+				builtin.lsp_type_definitions(lspOpts)
+			end
+			local lsp_references = function()
+				builtin.lsp_references(lspOpts)
+			end
+			local lsp_implementations = function()
+				builtin.lsp_implementations(lspOpts)
 			end
 
 			vim.keymap.set("n", "<leader>ff", find_files, { desc = "Files" })
@@ -66,9 +93,8 @@ return {
 			-- TODO: how to sort by last accessed here?
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
 			vim.keymap.set("n", "<leader>fr", builtin.registers, { desc = "Registers" })
-			vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Document symbols" })
-			-- vim.keymap.set("n", "<leader>fS", builtin.lsp_workspace_symbols, { desc = "Document symbols" })
-			vim.keymap.set("n", "<leader>fS", builtin.lsp_dynamic_workspace_symbols, { desc = "Workspace symbols" })
+			vim.keymap.set("n", "<leader>fs", lsp_document_symbols, { desc = "Document symbols" })
+			vim.keymap.set("n", "<leader>fS", lsp_dynamic_workspace_symbols, { desc = "Workspace symbols" })
 
 			vim.keymap.set(
 				"n",
@@ -83,10 +109,10 @@ return {
 				{ desc = "Grep selection" }
 			)
 
-			vim.keymap.set("n", "gd", builtin.lsp_definitions, { desc = "Go to definitions" })
-			vim.keymap.set("n", "gt", builtin.lsp_type_definitions, { desc = "Go to type definitions" })
-			vim.keymap.set("n", "gr", builtin.lsp_references, { desc = "Go to references" })
-			vim.keymap.set("n", "gi", builtin.lsp_implementations, { desc = "Go to implementations" })
+			vim.keymap.set("n", "gd", lsp_definitions, { desc = "Go to definitions" })
+			vim.keymap.set("n", "gt", lsp_type_definitions, { desc = "Go to type definitions" })
+			vim.keymap.set("n", "gr", lsp_references, { desc = "Go to references" })
+			vim.keymap.set("n", "gi", lsp_implementations, { desc = "Go to implementations" })
 
 			require("telescope").load_extension("live_grep_args")
 		end,
