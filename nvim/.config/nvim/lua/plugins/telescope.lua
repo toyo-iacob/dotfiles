287462e2f66d4afad374a400ed480ada20db8e9c
Theodor-Felix Iacob <toyo.iacob@gmail.com>
2024-11-28 14:39:48 +0200

    telescope project add cwd

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -17,6 +17,7 @@ return {
 			"folke/todo-comments.nvim",
 		},
 		config = function()
+			local project_actions = require("telescope._extensions.project.actions")
 			require("telescope").setup({
 				defaults = {
 					vimgrep_arguments = {
@@ -99,6 +100,36 @@ return {
 				extensions = {
 					live_grep_args = {
 						auto_quoting = true, -- enable/disable auto-quoting
+					},
+					project = {
+						sync_with_nvim_tree = true, -- default false
+						mappings = {
+							n = {
+								["d"] = project_actions.delete_project,
+								["r"] = project_actions.rename_project,
+								-- ["c"] = project_actions.add_project,
+								["c"] = project_actions.add_project_cwd,
+								["f"] = project_actions.find_project_files,
+								["b"] = project_actions.browse_project_files,
+								["s"] = project_actions.search_in_project_files,
+								["R"] = project_actions.recent_project_files,
+								["w"] = project_actions.change_working_directory,
+								["o"] = project_actions.next_cd_scope,
+							},
+							i = {
+								["<c-d>"] = project_actions.delete_project,
+								["<c-v>"] = project_actions.rename_project,
+								-- ["<c-a>"] = project_actions.add_project,
+								["<c-a>"] = project_actions.add_project_cwd,
+								["<c-f>"] = project_actions.find_project_files,
+								["<c-b>"] = project_actions.browse_project_files,
+								["<c-s>"] = project_actions.search_in_project_files,
+								["<c-r>"] = project_actions.recent_project_files,
+								["<c-l>"] = project_actions.change_working_directory,
+								["<c-o>"] = project_actions.next_cd_scope,
+								["<c-w>"] = project_actions.change_workspace,
+							},
+						},
 					},
 				},
 			})
